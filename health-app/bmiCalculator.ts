e97b965b62af91b3a1e7a249e77b525210b550bc
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2023-01-10 16:31:20 +0200

    Add semicolons

--- health-app/bmiCalculator.ts
+++ health-app/bmiCalculator.ts
@@ -1,24 +1,24 @@
 const calculateBmi = (height: number, weight: number): string => {
-    height = height / 100
-    const square = height * height
-    const bmi =  weight / square
+    height = height / 100;
+    const square = height * height;
+    const bmi =  weight / square;
 
     if (bmi < 16) {
-        return 'Underweight (severe thinness)'
+        return 'Underweight (severe thinness)';
     } else if (bmi < 17) {
-        return 'Underweight (moderate thinness)'
+        return 'Underweight (moderate thinness)';
     } else if (bmi < 18.5) {
-        return 'Underweight (mild thinness)'
+        return 'Underweight (mild thinness)';
     } else if (bmi < 25) {
-        return 'Normal (healthy weight)'
+        return 'Normal (healthy weight)';
     } else if (bmi < 30) {
-        return 'Overweight (pre-obese)'
+        return 'Overweight (pre-obese)';
     } else if (bmi < 35) {
-        return 'Obese (class I)'
+        return 'Obese (class I)';
     } else if (bmi < 40) {
-        return 'Obese (class II)'
+        return 'Obese (class II)';
     } else {
-        return 'Obese (class III)'
+        return 'Obese (class III)';
     }
 }
 
